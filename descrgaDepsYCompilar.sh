3295d8fa9f13c97afe5e9e03cba30fcf9121d156
Jorge <jorgepz@fing.edu.uy>
2020-10-18 13:11:26 -0300

    cambio script

--- descrgaDepsYCompilar.sh
+++ descrgaDepsYCompilar.sh
@@ -5,3 +5,5 @@ mv svg2pdftex.sh latex/
 
 wget https://gist.githubusercontent.com/jorgepz/9b3050d12ce9e79cdeac415b3845c715/raw/62f9e19cffde0e1521ba59d0144a9f3038af7cb6/definitions.tex 
 mv definitions.tex latex/texs/
+
+cd latex && latexmk -pdf libroResMat2.tex && cd ..
