1e0bead51f1311864be1fe6cc552231cf03505cb
Jorge Perez Zerpa <jorgepz@fing.edu.uy>
2020-10-19 09:20:40 -0300

    Update descrgaDepsYCompilar.sh

--- descrgaDepsYCompilar.sh
+++ descrgaDepsYCompilar.sh
@@ -1,7 +1,11 @@
 
-
 wget https://raw.githubusercontent.com/jorgepz/svg2pdf_tex/master/svg2pdftex.sh
 mv svg2pdftex.sh latex/
+
+cd latex
+chmod +x svg2pdf_tex.sh 1
+cd ..
+
 
 wget https://gist.githubusercontent.com/jorgepz/9b3050d12ce9e79cdeac415b3845c715/raw/62f9e19cffde0e1521ba59d0144a9f3038af7cb6/definitions.tex 
 mv definitions.tex latex/texs/
