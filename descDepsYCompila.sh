a54d6023665ea2e2fd3b905c7adcdfb79446be42
Jorge Perez Zerpa <jorgepz@fing.edu.uy>
2020-10-21 18:42:33 -0300

    Update descDepsYCompila.sh

--- descDepsYCompila.sh
+++ descDepsYCompila.sh
@@ -9,5 +9,5 @@ cd ..
 wget https://gist.githubusercontent.com/jorgepz/9b3050d12ce9e79cdeac415b3845c715/raw/62f9e19cffde0e1521ba59d0144a9f3038af7cb6/definitions.tex 
 mv definitions.tex latex/texs/
 
-cd latex && latexmk -pvc -pdf libroResMat2 && cd ..
+cd latex && latexmk -pdf libroResMat2 && cd ..
 #cd latex && pdflatex libroResMat2.tex && pdflatex libroResMat2.tex && cd ..
