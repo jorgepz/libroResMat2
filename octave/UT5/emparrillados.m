1be7f57dbf1cb463496557668eccbe2c6af42e86
Jorge <jorgepz@fing.edu.uy>
2022-08-01 17:29:50 -0300

    corrrecciones ut3 y muevo codigo ut5

--- octave/UT5/emparrillados.m
+++ octave/UT5/emparrillados.m
@@ -2,17 +2,13 @@
 % ejemplo de codigo para analisis de emparrillados usando a metodo matricial
 % Ejecucion en GNU-Octave - Octubre 2019 - Jorge Perez Zerpa, Ignacio Suarez, Bruno Bouchard
 % ----------------------------------------------------------------------
-
 close all, clear all
 
-l  = 2     ;  width = 0.05 ;
-
-E  = 210e9 ; nu = 0.3   ;   P  = 4e3   ;
-
+% parametros
+l  = 2 ;  width = 0.05 ;    E  = 210e9 ; nu = 0.3 ;   P  = 4e3   ;
 
 % modulo de corte
 G = E/(2*(1+nu)) ;
-
 % propiedades geometricas de seccion cuadrada
 J = 0.141*width^4 ; I  = width^4/12 ; A  = width^2    ;
 
@@ -21,12 +17,11 @@ Nodes = [ 0   l/2  ; ...
           l   l/2  ; ...
           l     0  ] ;
 
-%        n1 n2 mat sec          
-Conec = [ 1 2 ; ...
-          2 3 ] ;
+% conectividad: nodo1 nodo2
+Conec = [       1     2 ; ...
+                2     3 ] ;
 
 Angles = [ 0 -pi/2 ]' ;
-
 fixeddofs = [ 1 2 3 7 ];
 
 nelems = size( Conec,1);
@@ -36,9 +31,7 @@ freedofs  = (1:(3*nnodes));
 freedofs(fixeddofs) = [] ;
 
 KG      = sparse( 3*nnodes, 3*nnodes ) ;
- 
 for i = 1:nelems
-
   alphay = Angles(i);  ca = cos(alphay); sa = sin(alphay);
   
   R = [ ca  0 -sa  0  0   0 ; ...
@@ -51,8 +44,7 @@ for i = 1:nelems
   elemNodes = Conec( i,:);
   lelem     = norm( Nodes( elemNodes(2),:) - Nodes( elemNodes(1),:) ) ;
   
-  KL = zeros(6,6);
-  
+  KL = zeros(6,6);  
   KL([1 4], [1 4])         = G*J/lelem * [ 1 -1 ; ...
                                           -1  1 ] ; 
 
@@ -74,22 +66,16 @@ KG(fixeddofs, :) = [] ; KG(:, fixeddofs) = [] ;
 FG( fixeddofs  ) = [] ;
 
 % resolucion de sistema
-u = KG\FG ;
-UG = zeros( 3*nnodes,1);
-UG(freedofs ) = u 
+u = KG\FG ;    UG = zeros( 3*nnodes,1);    UG(freedofs ) = u 
 
 %ploteo de parrillado
-figure
-hold on, grid on
+figure, hold on, grid on
 diameterStructure = max(max( Nodes))  - min(min(Nodes)) ;
 margen = 0.1 * diameterStructure ;
-
 for i=1:nelems
-  nodesElem = Conec(i,:) 
+  nodesElem = Conec(i,:) ;
   p1 = Nodes( nodesElem(1),:) 
   p2 = Nodes( nodesElem(2),:) 
   plot3( [p1(1)  p2(1)], [p1(2)  p2(2)], [0 0] ,'b')
 end
 view(3)
-
-
